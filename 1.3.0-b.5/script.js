c1722b5e3b6f4a54eafc7b238bae301f9e581762
coding-unless <122762802+coding-unless@users.noreply.github.com>
2023-01-16 13:05:58 +0800

    Update script.js

--- 1.3.0-b.5/script.js
+++ 1.3.0-b.5/script.js
@@ -1,1 +1,24 @@
+const log = console.log;
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function Loaded() {
+    log("Debug: Body is loaded");
+}
+
+function genTable() {
+    var table;
+    table='<table id="mt">';
+    var num1=document.getElementById("InputNum").value;
+    for(i=1;i<=num1;i++){
+        table+='<tr>';
+        for(let num=1;num<=num1;num++){
+            table+='<td>'+num*i+'</td>';
+        }
+        table+='</tr>';
+    }
+    table+='</table>';
+    document.getElementById("thetable").innerHTML = table;
+}
